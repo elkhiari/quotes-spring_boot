9694a47ebe38bdcc82deb18c8dda3f01131699cb
Othmane elkhiari <othmaneelkhiari@gmail.com>
2023-08-30 07:41:38 +0100

    handle small error in random id

--- src/main/java/com/quotes/quotes/quotes/QuotesService.java
+++ src/main/java/com/quotes/quotes/quotes/QuotesService.java
@@ -71,13 +71,12 @@ public class QuotesService {
 
     public Quotes getRandomQuotes() {
         long count = repository.count();
+        System.out.println(count);
         if (count == 0 ) throw new IllegalArgumentException("No Quotes found in database");
-        Quotes quotes = repository.findById(
-                ThreadLocalRandom.current().nextLong(0,count - 1)
-        ).orElseThrow(
+        long number = ThreadLocalRandom.current().nextLong(1,count);
+        System.out.println(number);
+        return repository.findById(number).orElseThrow(
                 () -> new IllegalStateException("Internal Server Error")
         );
-
-        return quotes;
     }
 }
